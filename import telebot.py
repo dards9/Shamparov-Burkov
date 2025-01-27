51ffe8a2a032a377954d90f35e1a1423245e53bc
dards9 <world.kirill@bk.ru>
2025-01-27 15:49:50 +0400

    Update import telebot.py

--- import telebot.py
+++ import telebot.py
@@ -1,8 +1,26 @@
 import telebot
 from telebot import types
+import logging
+import os
+import openai
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import ContentType, File, Message
+from aiogram.utils import executor
+from dotenv import load_dotenv
+from moviepy.editor import VideoFileClip
+from pydub import AudioSegment
+load_dotenv()
 
 token = '7790178826:AAHzJIiCi9i6nS2-4xIqm74K6CuAcSPcJuY'
 bot = telebot.TeleBot(token)
+PATH_SAVE_VOICE = 'data/voice/'
+PATH_SAVE_VIDEO_NOTE = 'data/video_note/'
+logging.basicConfig(level=logging.INFO)
+PATH_SAVE_VOICE = 'data/voice/'
+PATH_SAVE_VIDEO_NOTE = 'data/video_note/'
+
+logging.basicConfig(level=logging.INFO)
+
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -11,7 +29,7 @@ def start_message(message):
 @bot.message_handler(commands=['button'])
 def button(message):
     markup = types.InlineKeyboardMarkup(row_width=1)
-    item = types.InlineKeyboardButton('ваше голосове сообщение', callback_data='question_1')
+    item = types.InlineKeyboardButton('Ваше голосове сообщение', callback_data='question_1')
     markup.add(item)
     bot.send_message(message.chat.id, 'Нажмите кнопку:', reply_markup=markup)
 
