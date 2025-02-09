80e1d576eb9ef24567a66d40b0d8df58cee888ab
dards9 <world.kirill@bk.ru>
2025-02-09 11:52:46 +0400

    Update main.py

--- BotTG/main.py
+++ BotTG/main.py
@@ -1,10 +1,13 @@
 import whisper
 from fastapi import FastAPI,Request
 import uvicorn
-import aiophht
+import aiohttp
+import aiofiles
+TG_API='7790178826:AAHzJIiCi9i6nS2-4xIqm74K6CuAcSPcJuY'
+
 
 from schemas import Answer
-from swh import Answer
+
 
 model = whisper.load_model("small")
 result = model.transcribe("./text.mp3")
@@ -15,19 +18,35 @@ app=FastAPI()
 @app.post("/")
 async def read_root(request:Request):
     json=await request.json()
-    print(json)
-    odj=Answer.model_viledate(json) 
+    #print(json)
+    odj=Answer.model_validate(json) 
 
     file_id = odj.message.voise.file_id
-    
-    'https://api.telegram.org/bot{TG_API}/getFile'
+
     data={'file_id': file_id}
      
-    async with aiophht.ClientSession() as session:
-    
+    async with aiohttp.ClientSession() as session:
+        async with session.post(f'https://api.telegram.org/bot{TG_API}/getFile', data= data) as reponse:
+          resfileinfo=await reponse.json() 
+          print(resfileinfo)
+          if resfileinfo.get('ok'):
+            path=resfileinfo['result']['file_path']
+            ext = path.split('.')[-1]
+            async with session.get(f'https://api.telegram.org/file/bot{TG_API}/{path}') as dwfile:
+                async with aiofiles.open('filename',mode='wb') as f :
+                   content=await dwfile.read()
+                   await f.write(content)
+
+
+
+
+
+
+
+
+
 
 
 
     return 200
-
     
