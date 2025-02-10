3b213695476c212a571f364d4439cab50d8fd9f5
dards9 <world.kirill@bk.ru>
2025-02-10 23:35:06 +0400

    Delete swh.py

--- swh.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-TG_API='7790178826:AAHzJIiCi9i6nS2-4xIqm74K6CuAcSPcJuY'
-whook='aae23ea60dda4e.Ihr.life'
-
-r= requests.get(f'https://api.telegram.org/bot{TG_API}/setWebhook?url=https://{whook}/')
-
-print(r.json())
